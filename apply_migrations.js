8396dbb0094a70a6fe6c9cd069e2cbc41bc910f1
Vitor Miguel <vitorlb2125@gmail.com>
2026-05-20 15:57:11 -0300

    fix: add tenants.atualizado_em column (missing migration)
    PATCH /v1/configuracoes/ranking-publico crashing with 500 in prod:
      column "atualizado_em" of relation "tenants" does not exist
    
    Migration adds column + BEFORE UPDATE trigger to keep it fresh.

--- apply_migrations.js
+++ apply_migrations.js
@@ -86,6 +86,7 @@ export const MIGRATIONS_LIST = [
   '089_ranking_publico_config.sql',
   '090_comissao_metas_compat.sql',
   '091_lives_agenda_link.sql',
+  '092_tenants_atualizado_em.sql',
 ]
 
 async function ensureMigrationsTable(client) {
